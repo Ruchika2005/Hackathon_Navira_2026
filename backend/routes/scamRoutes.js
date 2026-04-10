7d0b81cc070eb61e45c09c862fd7d51ec5e0ace3
Kanchan Jadhav <kanchanjadhav908@gmail.com>
2026-04-11 02:02:35 +0530

    Ignore inner repository

--- backend/routes/scamRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { scamChat } = require('../controllers/scamController');
-
-router.post('/chat', scamChat);
-
-module.exports = router;
